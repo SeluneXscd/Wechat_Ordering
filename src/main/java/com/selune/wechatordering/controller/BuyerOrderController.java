4dbb394377bb11d6c15914eb16cb085d00af92d7
SeluneXscd <1353977352@qq.com>
2019-05-20 09:22:42 +0800

    fix: add BuyerOrderController

---
+++ src/main/java/com/selune/wechatordering/controller/BuyerOrderController.java
@@ -0,0 +1,27 @@
+package com.selune.wechatordering.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Selune
+ * @Date: 5/19/19 6:37 PM
+ */
+
+@Slf4j
+@RestController
+@RequestMapping("/buyer/order")
+public class BuyerOrderController {
+
+    // 创建订单
+
+
+    // 订单列表
+
+    // 订单详情
+
+    // 取消订单
+
+
+}
