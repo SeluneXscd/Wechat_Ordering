b545ce950a1043f787a7a598f95f1903d3f36b93
SeluneXscd <1353977352@qq.com>
2019-07-08 11:09:49 +0800

    fix: add SellerInfo pojo

---
+++ src/main/java/com/selune/wechatordering/pojo/SellerInfo.java
@@ -0,0 +1,27 @@
+package com.selune.wechatordering.pojo;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @Author: Selune
+ * @Date: 7/8/19 11:01 AM
+ */
+
+@Entity
+@Table(name = "seller_info")
+@Data
+public class SellerInfo {
+
+    @Id
+    private String sellerId;
+
+    private String username;
+
+    private String password;
+
+    private String openId;
+}
