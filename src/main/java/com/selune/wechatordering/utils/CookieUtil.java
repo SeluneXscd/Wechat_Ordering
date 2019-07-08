a3105f2c113d14bbc4ea7932f5ca990c532697ba
SeluneXscd <1353977352@qq.com>
2019-07-08 22:14:26 +0800

    fix: update CookieUtil

--- src/main/java/com/selune/wechatordering/utils/CookieUtil.java
+++ src/main/java/com/selune/wechatordering/utils/CookieUtil.java
@@ -1,7 +1,10 @@
 package com.selune.wechatordering.utils;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Cookie 工具类
@@ -29,7 +32,27 @@ public class CookieUtil {
         response.addCookie(cookie);
     }
 
-    public static void get() {
+    /**
+     * 将Cookie封装成Map
+     * @param request
+     * @param name
+     * @return
+     */
+    public static Cookie get(HttpServletRequest request,
+                           String name) {
 
+        Map<String, Cookie> cookieMap = readCookieMap(request);
+        return cookieMap.getOrDefault(name, null);
+    }
+
+    private static Map<String, Cookie> readCookieMap(HttpServletRequest request) {
+        Map<String, Cookie> cookieMap = new HashMap<>();
+        Cookie[] cookies = request.getCookies();
+        if (null != cookies) {
+            for (Cookie cookie : cookies) {
+                cookieMap.put(cookie.getName(), cookie);
+            }
+        }
+        return cookieMap;
     }
 }
