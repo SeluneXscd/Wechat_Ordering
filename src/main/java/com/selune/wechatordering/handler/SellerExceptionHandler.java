fa957bfeb12e18a3d79aee22806a8cb6a35cd1fc
SeluneXscd <1353977352@qq.com>
2019-07-09 10:44:04 +0800

    fix: update SellerExceptionHandler 自定义异常捕获

--- src/main/java/com/selune/wechatordering/handler/SellerExceptionHandler.java
+++ src/main/java/com/selune/wechatordering/handler/SellerExceptionHandler.java
@@ -2,9 +2,15 @@ package com.selune.wechatordering.handler;
 
 import com.selune.wechatordering.config.ProjectUrlConfig;
 import com.selune.wechatordering.exception.SellerAuthorizeException;
+import com.selune.wechatordering.exception.WeChatOrderException;
+import com.selune.wechatordering.utils.ResultVOUtil;
+import com.selune.wechatordering.vo.ResultVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -28,5 +34,13 @@ public class SellerExceptionHandler {
                 .concat("/sell/wechat/qrAuthorize")
                 .concat("?returnUrl=")
                 .concat("/sell/seller/login"));
+    }
+
+    // 异常统一处理
+    @ExceptionHandler(value = WeChatOrderException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.FORBIDDEN) // 定义返回HTTP状态码
+    public ResultVO handlerWeChatOrderException(WeChatOrderException e) {
+        return ResultVOUtil.error(e.getCode(), e.getMessage());
     }
 }
