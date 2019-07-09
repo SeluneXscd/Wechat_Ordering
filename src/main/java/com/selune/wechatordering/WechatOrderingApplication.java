f572fd361bb2f8bc2cccc51e35e46d922bf9c7cc
SeluneXscd <1353977352@qq.com>
2019-07-09 15:26:19 +0800

    fix: add MyBatis MyBatis注解方式

--- src/main/java/com/selune/wechatordering/WechatOrderingApplication.java
+++ src/main/java/com/selune/wechatordering/WechatOrderingApplication.java
@@ -1,9 +1,11 @@
 package com.selune.wechatordering;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.selune.wechatordering.pojo.mapper")
 public class WechatOrderingApplication {
 
 	public static void main(String[] args) {
